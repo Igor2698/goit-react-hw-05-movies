6aeeeda56b68ab51b21147aaa2877fb2064cedc4
Igor2698 <128493043+Igor2698@users.noreply.github.com>
2023-09-13 13:33:26 +0300

    +

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,13 @@
 export const NotFound = () => {
-  return <div>Did not find your page</div>;
+  return (
+    <div>
+      <p>Unfortunately, an error occurred. Please try again later</p>
+      <img
+        width="80%"
+        height="600"
+        src="https://png.pngtree.com/background/20211217/original/pngtree-system-error-background-picture-image_1599770.jpg"
+        alt="error"
+      />
+    </div>
+  );
 };
